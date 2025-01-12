1bae3b08ef1d12c320dbf5be6505b8ab485f552a
Bry Salvador <brysalvador@hotmail.com>
2025-01-12 16:39:29 +0800

    Add project files.

---
+++ ReviewApp/Interfaces/IPokemonRepository.cs
@@ -0,0 +1,14 @@
+﻿using ReviewApp.Models;
+
+namespace ReviewApp.Interfaces
+{
+    public interface IPokemonRepository
+    {
+        ICollection<Pokemon> GetPokemons();
+        Pokemon GetPokemon(int id);
+        Pokemon GetPokemon(string name);
+        Pokemon GetPokemon(Pokemon pokemon);
+        decimal GetPokemonRating(int pokeId);
+        bool PokemonExists(int pokeId);
+    }
+}
---
+++ ReviewApp/Repository/PokemonRepository.cs
@@ -0,0 +1,48 @@
+﻿using ReviewApp.Database;
+using ReviewApp.Interfaces;
+using ReviewApp.Models;
+
+namespace ReviewApp.Repository
+{
+    public class PokemonRepository : IPokemonRepository
+
+    {
+        private readonly DatabaseContext _databaseContext;
+        public PokemonRepository(DatabaseContext context)
+        {
+            _databaseContext = context;
+        }
+
+        public Pokemon GetPokemon(int id)
+        {
+            return _databaseContext.Pokemon.Where(p => p.Id == id).FirstOrDefault();
+        }
+
+        public Pokemon GetPokemon(string name)
+        {
+            return _databaseContext.Pokemon.Where(p => p.Name == name).FirstOrDefault();
+        }
+
+        public decimal GetPokemonRating(int pokeId)
+        {
+            var review = _databaseContext.Reviews.Where(p => p.Pokemon.Id == pokeId);
+            if (review.Count() <= 0)
+                return 0;
+            return ((decimal)review.Sum(r => r.Rating) / review.Count());
+        }
+        public bool PokemonExists(int pokeId)
+        {
+            return _databaseContext.Pokemon.Any(p => p.Id == pokeId);
+        }
+        public Pokemon GetPokemon(Pokemon pokemon)
+        {
+            throw new NotImplementedException();
+        }
+
+        public ICollection<Pokemon> GetPokemons()
+        {
+            return _databaseContext.Pokemon.OrderBy(p => p.Id).ToList();
+        }
+
+    }
+}
